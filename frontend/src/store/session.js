8750c37aae1f12800cc735b5a30ac7e34f5c8c02
MattBoyer <matthewryanboyer@yahoo.com>
2023-06-20 16:03:46 -0700

    finished phase 4

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -38,6 +38,30 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+export const logout = () => async (dispatch) => {
+  const response = await csrfFetch('/api/session', {
+    method: 'DELETE',
+  });
+  dispatch(removeUser());
+  return response;
+};
+
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstName,
+      lastName,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
@@ -55,5 +79,6 @@ const sessionReducer = (state = initialState, action) => {
       return state;
   }
 };
+
 
 export default sessionReducer;
